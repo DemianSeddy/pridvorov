3e662e1b66aa716fffee8e2d0efc08d3e9ce5430
Alexey Pridvorov <pridvorov@gmail.com>
2018-03-07 15:24:06 +0700

    fff

--- js/js/deskrow.js
+++ js/js/deskrow.js
@@ -1,4 +1,16 @@
 var arr = ["black", "white"];
+
+/*function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+var timerId = setTimeout(function tick() {
+	var xy = Math.round(getRandomArbitrary(11,88)+11);
+	el=document.getElementById("cell"+xy); 
+	console.log("cell"+xy+":"+arr[(xy)%2]);
+	el.style.background=arr[(xy)%2];
+	timerId = setTimeout(tick, 2000);
+}, 2000);*/
  
  var row=8;
  for(j=row;j>0;j--)
@@ -6,29 +18,33 @@ var arr = ["black", "white"];
     document.write('<div class = "parent_header">');
     document.write('<div class = "child_header_char"><h5>'+j+'</h5></div>');
     col=row+1;
-    for(i=1;i<col;i++)
-       {if ((j+i)%2==1) {
-               var img="boxchessblack.png";
-				  }
-          else { 
-               var img="boxchesswhite.png";
-          }
-          //document.write('<div name="cell'+j+i+'" class="child_header"><h5>'+(j+i)%2+'</h5/></div>');
-		  //Вообще хотел вот так
-		  //document.getElementById("cell12").style.background="+arr[(j+i)%2]+";
-		  
-          document.write('<div id="cell'+j+i+'" name="cell'+j+i+'" class="child_header"><img src="img/chess/'+img+'"/></div>');
-		  
-          //if (j==2){
-          //    var imgfig='wP.png';
-          //    document.write('<div id="cell'+j+i+'" class="child_header"><img src="img/chess/'+imgfig+'"/></div>');
-          //}
+    for(i=1;i<col;i++){
+          document.write('<div id="cell'+j+i+'" class="child_header"><h5>'+(j+i)%2+'</h5/></div>');
        }
      document.write('<div class = "child_header_char mirrorY"><h5>'+j+'</h5></div>');
      document.write('</div>');
 				  
 	 }
-      	 console.log(document.getElementById("cell12").style.border); 
+	 
+	
+
+
+	for(j=row;j>0;j--){ 
+		 for(i=1;i<col;i++){
+		     el=document.getElementById("cell"+j+i); 
+			 el.style.border="double";
+			 if ((j+i)%2==0) {
+				  el.style.background=arr[0];
+				  el.style.fontcolor=arr[1];}
+				 else 
+				 {
+                  el.style.background=arr[1];
+				  el.style.fontcolor=arr[0];
+				  }					 
+			 if (j==2) {el.h5.text="&#9823";}	  
+			}
+		}
+		
 	 
 
 
