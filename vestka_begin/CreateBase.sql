8461218f5acc16aed059c7c59195c19935afc0b9
Alexey Pridvorov <pridvorov@gmail.com>
2018-03-07 10:20:05 +0700

    delete

--- vestka_begin/CreateBase.sql
+++
@@ -1,6 +0,0 @@
-/*create database CHARACTER SET utf8 COLLATE utf8_general_ci if not tbase;
-*/
-
-#insert into `animals` values(null,'Первый','3.163858404','14.09.16 14:01')
-select supplier,summ,d_t from animals order by id desc
-#delete from animals
